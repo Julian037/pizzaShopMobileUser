5ebac4e985b4f5abc3043b0e5368f987b62d7a3f
Julian037 <jv.julian1313@gmail.com>
2024-03-02 23:15:22 -0500

    finished the initial structure of the signin component

--- src/app/screens/Signin/SignInStyle.jsx
+++ src/app/screens/Signin/SignInStyle.jsx
@@ -1,50 +1,11 @@
 import { Dimensions, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   paddingHorizontal: '3%',
-  //   justifyContent: 'center',
-  // },
-  // logoContainer: {
-  //   alignItems: 'center',
-  //   marginBottom: 15,
-  // },
-  // box: {
-  //   backgroundColor: 'white',
-  //   borderRadius: 12,
-  //   paddingHorizontal: 20,
-  //   paddingVertical: 40,
-  //   elevation: 2,
-  //   marginBottom: 10,
-  // },
-  // logo: {
-  //   width: '100%',
-  //   resizeMode: 'contain',
-  // },
-  // emailLabel: {
-  //   color: '#585858',
-  //   marginBottom: 5,
-  // },
-  // emailInput: {
-  //   borderBottomWidth: 0.7,
-  //   borderColor: '#D0D0D0',
-  //   fontSize: 16,
-  // },
-  // button: {
-  //   marginTop: 15,
-  // },
-  // infoItemLabel: {
-  //   textDecorationLine: 'underline',
-  // },
-
   content:{
+    paddingTop: 50,
     flex: 1,
     paddingHorizontal: 30,
     borderTopRightRadius: 30,
-    // backgroundColor: 'red',
-    // backgroundColor: '#FFFFFF',
-    // marginTop: -25,
   },
   imgContainer: {
     alignSelf: 'center',
@@ -54,9 +15,16 @@ const styles = StyleSheet.create({
     zIndex: 1,
     position: 'absolute',
     alignSelf: 'center',
-    // marginTop: 20,
     height: '100%',
     width: '100%',
+  },
+  link: {
+    fontWeight: 'bold',
+    color: '#2EA1E8',
+    textDecorationLine: 'underline',
+  },
+  text: {
+    marginTop: 6,
   },
 });
 
